f506490f00268999028b424bf8911023254af0e6
Andrew <70990567+apakula036@users.noreply.github.com>
2022-10-19 21:05:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import curses 
 from curses import wrapper 
 
+
+# stdscr is the screen use that to manipulate it 
 def main(stdscr): # standard output screen 
+    stdscr.clear()
     stdscr.addstr("Hello world!")
-    
+    stdscr.refresh()
+
+wrapper(main)
